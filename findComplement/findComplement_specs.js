c1511740ba4ade930231921bd44a29093d08f5d5
Thanh-Lai <tlai1122@gmail.com>
2018-09-05 19:26:48 -0400

    changed let to const

--- findComplement/findComplement_specs.js
+++ findComplement/findComplement_specs.js
@@ -1,16 +1,16 @@
 describe('findComplement', function(){
     it('returns a number', function(){
-        let output = findComplement(10)
+        const output = findComplement(10)
         expect(typeof output === 'number').toEqual(true)
     });
 
     it('should return 7 if parameter is 8', function(){
-        let output = findComplement(8)
+        const output = findComplement(8)
         expect(output).toEqual(7)
     })
 
     it('should return 9 if parameter is 22', function(){
-        let output = findComplement(22)
+        const output = findComplement(22)
         expect(output).toEqual(9)
     })
 });
